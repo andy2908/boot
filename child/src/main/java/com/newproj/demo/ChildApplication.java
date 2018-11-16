eb46cf8aeed2b83755ec829c6513757ffaa547bf
Admin <Admin@DESKTOP-JUKCHCM>
2018-11-16 11:48:31 +0530

    test

---
+++ child/src/main/java/com/newproj/demo/ChildApplication.java
@@ -0,0 +1,12 @@
+package com.newproj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages ="com.newproj.demo")
+public class ChildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildApplication.class, args);
+	}
+}
